e05bc4e64f198dad2c7170f4e8c985bc8c97303a
alperencubuk <alperencubuk@outlook.com>
2023-04-13 14:33:10 +0300

    Test config

--- test/boilerplate/test_boilerplate_views.py
+++ test/boilerplate/test_boilerplate_views.py
@@ -1,29 +1,41 @@
 from datetime import datetime
-from test.conftest import database
 
 import pytest
 from httpx import AsyncClient
+from motor.motor_asyncio import AsyncIOMotorDatabase
 
 
-@pytest.mark.anyio
+@pytest.mark.asyncio
 async def test_create_boilerplate(client: AsyncClient):
-    response = await client.post(
-        "/boilerplate/",
-        json={"email": "test_create@boilerplate.com"},
-    )
-    assert response.status_code == 201
-    assert response.json()["email"] == "test_create@boilerplate.com"
-
-
-@pytest.mark.anyio
-async def test_get_boilerplate(client: AsyncClient):
-    boilerplate = {
-        "email": "test_get@boilerplate.com",
-        "create_date": datetime.utcnow(),
-        "update_date": datetime.utcnow(),
+    payload = {
+        "email": "test_create@boilerplate.com",
+        "first_name": "boiler",
+        "last_name": "plate",
     }
-    new_boilerplate = await database["boilerplate"].insert_one(boilerplate)
+    response = await client.post("/boilerplate/", json=payload)
 
+    assert response.status_code == 201
+    data = response.json()
+    assert data["email"] == payload["email"]
+    assert data["first_name"] == payload["first_name"]
+    assert data["last_name"] == payload["last_name"]
+
+
+@pytest.mark.asyncio
+async def test_get_boilerplate(client: AsyncClient, db: AsyncIOMotorDatabase):
+    date = datetime.utcnow()
+    payload = {
+        "email": "test_get@boilerplate.com",
+        "first_name": "boiler",
+        "last_name": "plate",
+        "create_date": date,
+        "update_date": date,
+    }
+    new_boilerplate = await db["boilerplate"].insert_one(payload)
     response = await client.get(f"/boilerplate/{new_boilerplate.inserted_id}")
+
     assert response.status_code == 200
-    assert response.json()["email"] == "test_get@boilerplate.com"
+    data = response.json()
+    assert data["email"] == payload["email"]
+    assert data["first_name"] == payload["first_name"]
+    assert data["last_name"] == payload["last_name"]
--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,6 @@
-import pytest
+import asyncio
+
+import pytest_asyncio
 from httpx import AsyncClient
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
@@ -8,26 +10,30 @@ from source.main import app
 
 
 def get_test_db() -> AsyncIOMotorDatabase:
-    return mongo_client.test
-
-
-database = get_test_db()
+    return mongo_client.test_database
 
 
 app.dependency_overrides[get_db] = get_test_db
 
 
-@pytest.fixture(scope="session")
-def anyio_backend():
-    return "asyncio"
-
-
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 async def client():
     async with AsyncClient(
         app=app,
         base_url="http://test",
         headers={settings.API_KEY_HEADER: settings.API_KEY},
-    ) as client:
-        yield client
-    await mongo_client.drop_database("test")
+    ) as async_client:
+        yield async_client
+
+
+@pytest_asyncio.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def db():
+    yield get_test_db()
+    await mongo_client.drop_database("test_database")
